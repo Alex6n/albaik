016b645216083c690bd10ef8eda7063c3014ea60
Alex6n <hheikal.work@gmail.com>
2024-01-06 06:11:23 +0200

    Update global styles and header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,39 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="border-b border-zinc-300 flex flex-cols-3 justify-between align-middle gap-10 mx-[15%] my-2 bg-black/5">
+    <div className="border-b border-zinc-300/50 flex flex-cols-3 justify-between items-center gap-10 mx-[15%] my-5">
       <div>
-        <Image src="/logo_en.png" alt="logo" width={100} height={100} />
+        <Link href="/">
+          <Image src="/logo_en.png" alt="logo" width={130} height={130} />
+        </Link>
       </div>
-      <div>b</div>
-      <div>c</div>
+
+      <div className="flex gap-12 font-semibold text-md">
+        <Link
+          href="#"
+          className="hover:bg-gradient-to-r hover:from-primary hover:to-secondary inline-block hover:text-transparent bg-clip-text transition-all"
+        >
+          Our Story
+        </Link>
+        <Link
+          href="#"
+          className="hover:bg-gradient-to-r hover:from-primary hover:to-secondary inline-block hover:text-transparent bg-clip-text transition-all"
+        >
+          Our Purpose
+        </Link>
+        <Link
+          href="#"
+          className="hover:bg-gradient-to-r hover:from-primary hover:to-secondary inline-block hover:text-transparent bg-clip-text transition-all"
+        >
+          Our Menu
+        </Link>
+      </div>
+
+      <div className="rounded-3xl border-zinc-400 border px-2 pb-1 font-semibold transition-all hover:shadow-md">
+        <Link href="#">عربي</Link>
+      </div>
     </div>
   );
 };
