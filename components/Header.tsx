0b3b8b6313ef490f912b6301f2f34a28f7e8516d
Alex6n <hheikal.work@gmail.com>
2024-01-08 03:46:28 +0200

    Responsive header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="border-b border-zinc-300/50 flex flex-cols-3 justify-between items-center gap-10 mx-[15%] my-5">
-      <div>
+    <div className="border-b border-zinc-300/50 md:flex md:flex-cols-3 justify-between items-center gap-10 mx-[15%] my-5">
+      <div className="mx-[40%] md:mx-0">
         <Link href="/">
           <Image src="/logo_en.png" alt="logo" width={130} height={130} />
         </Link>
       </div>
 
-      <div className="flex gap-12 font-semibold text-md">
+      <div className="flex gap-12 font-semibold text-md text-center my-[10%] mx-[15%] md:mx-0 md:my-0">
         <Link
           href="#"
           className="hover:bg-gradient-to-r hover:from-primary hover:to-secondary inline-block hover:text-transparent bg-clip-text transition-all"
@@ -31,7 +31,7 @@ const Header = () => {
         </Link>
       </div>
 
-      <div className="rounded-3xl border-zinc-400 border px-2 pb-1 font-semibold transition-all hover:shadow-md">
+      <div className="rounded-3xl border-zinc-400 border px-2 pb-1 font-semibold transition-all hover:shadow-md text-center">
         <Link href="#">عربي</Link>
       </div>
     </div>
