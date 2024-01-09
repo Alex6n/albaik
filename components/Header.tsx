f5b6f559972dee6b542f9c49e3db2c387275ac88
Alex6n <hheikal.work@gmail.com>
2024-01-09 09:51:14 +0200

    Changing style and Refactor

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,47 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const Header = () => {
+// TODO: Change Language
+interface HeaderProps {
+  navItems: {
+    label: string;
+    href: string;
+  }[];
+}
+
+const Header = ({ navItems }: HeaderProps) => {
   return (
-    <div className="md:border-b border-zinc-300/50 md:flex md:flex-cols-3 justify-between items-center gap-10 mx-[15%] my-5">
-      <div className="flex w-full justify-center md:block">
-        <Link href="/">
-          <Image src="/logo_en.png" alt="logo" width={130} height={130} />
-        </Link>
-      </div>
+    <>
+      <div className="bg-[#CE3737] w-full h-[25%] md:h-[12%] absolute z-[-100] top-0" />
+      <div className="md:flex md:flex-cols-3 justify-between items-center gap-10 mx-[15%] my-5">
+        <div className="flex w-full justify-center md:block">
+          <Link href="/">
+            <Image src="/logo_en.png" alt="logo" width={130} height={130} />
+          </Link>
+        </div>
 
-      <div className="justify-center font-bold flex gap-12 text-md text-center my-[10%] mx-[15%] md:mx-0 md:my-0">
-        <Link
-          href="#"
-          className="bg-zinc-900 hover:bg-gradient-to-r hover:from-primary hover:to-secondary inline-block text-transparent bg-clip-text transition-colors duration-300 ease-in-out"
-        >
-          Story
-        </Link>
-        <Link
-          href="#"
-          className="hover:bg-gradient-to-r hover:from-primary hover:to-secondary inline-block hover:text-transparent bg-clip-text transition-all"
-        >
-          Purpose
-        </Link>
-        <Link
-          href="#"
-          className="hover:bg-gradient-to-r hover:from-primary hover:to-secondary inline-block hover:text-transparent bg-clip-text transition-all"
-        >
-          Menu
-        </Link>
-      </div>
+        <div className="justify-center font-bold flex gap-12 text-md text-center m-[5%] md:m-0">
+          {navItems.map((navItem) => (
+            <Link
+              key={navItem.label}
+              href={navItem.href}
+              className="text-white duration-700 hover:bg-gradient-to-r from-primary to-secondary inline-block hover:text-transparent bg-clip-text transition-all tracking-wide"
+            >
+              {navItem.label}
+            </Link>
+          ))}
+        </div>
 
-      <div className="rounded-3xl border-zinc-400 border px-2 pb-1 font-semibold transition-all hover:shadow-md text-center">
-        <Link href="#">عربي</Link>
+        <div className="flex justify-center w-full md:w-fit">
+          <Link
+            className="rounded-3xl border-zinc-300 border pb-1 font-semibold transition-all duration-700 hover:shadow-lg text-center text-white max-w-fit px-10"
+            href="#"
+          >
+            عربي
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
