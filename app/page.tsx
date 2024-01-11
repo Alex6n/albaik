ab9c3205c0c8e95adf052833bca72d5bdfc79b02
Alex6n <hheikal.work@gmail.com>
2024-01-11 11:58:34 +0200

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import Button from "@/components/ui/Button";
 
 // TODO : consider making the bg color of the fist section red
 // TODO : copy mobile app functionality
@@ -7,7 +8,19 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <main>
-      <Hero />
+      <Hero
+        title="Savor the Spice, Embrace the Crisp!"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+            beatae suscipit voluptatibus consequuntur dolore. Inventore nostrum
+            tenetur asperiores consectetur nemo."
+        bgImage="/home_hero.jpg"
+      >
+        {/* <Button size="lg">Explore the Menu</Button>
+        <Button size="lg" primary={false}>
+          Our Core Values
+        </Button> */}
+      </Hero>
+      {/* TODO: Values */}
     </main>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,34 +1,33 @@
 import Image from "next/image";
-import Button from "./ui/Button";
-import { Drumstick } from "lucide-react";
 
-const Hero = () => {
+interface HeroProps {
+  title: string;
+  description: string;
+  bgImage: string;
+  children?: React.ReactNode;
+}
+
+const Hero = ({ title, description, bgImage, children }: HeroProps) => {
   return (
     <div className="relative">
       <div className="w-full h-[90vh] top-0 absolute bg-gray-900/50 justify-center">
         <div className="w-full flex justify-center">
           <h1 className="text-background text-center mt-36 leading-snug text-5xl md:text-[115px] font-bold tracking-tight max-w-5xl albaik">
-            Savor the Spice, Embrace the Crisp!
+            {title}
           </h1>
         </div>
         <div className="w-full flex justify-center">
           <p className="text-zinc-200 text-center max-w-2xl italic font-semibold text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-            beatae suscipit voluptatibus consequuntur dolore. Inventore nostrum
-            tenetur asperiores consectetur nemo.
+            {description}
           </p>
         </div>
-        <div className="hidden w-full flex flex-cols-2 justify-center my-2 albaik">
-          <Button size="lg">Explore the Menu</Button>
-          <Button size="lg" primary={false}>
-            Our Core Values
-          </Button>
+        <div className="w-full flex flex-cols-2 justify-center my-2 albaik">
+          {children}
         </div>
-        {/* TODO: Values */}
       </div>
       <Image
-        src="/home_hero.jpg"
-        alt="home_hero"
+        src={bgImage}
+        alt={bgImage}
         width={1000}
         height={1000}
         className="object-cover w-full h-[90vh]"
