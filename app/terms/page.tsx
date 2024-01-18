abd4806010b40a50793e5897d53e3cd698fc0018
Alex6n <hheikal.work@gmail.com>
2024-01-18 23:37:20 +0200

    Terms Layout

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,5 +1,351 @@
+import DataCard from "@/components/DataCard";
+import data from "../../public/terms.json";
+
 const page = () => {
-  return <div>terms page</div>;
+  return (
+    <div className="min-h-[53.9vh]">
+      <div className="bg-[#CE3737]/90 py-10 text-background font-bold text-center text-7xl">
+        Terms & Conditions
+      </div>
+
+      <DataCard>
+        <h2>PLEASE READ THE FOLLOWING TERMS AND CONDITIONS CAREFULLY</h2>
+        <p>
+          By using the pages in this site, you agree to these terms and
+          conditions. If you do not agree, you should not use this site.
+        </p>
+
+        <h1>Key Definitions</h1>
+        <ul>
+          <li>
+            Website: The ALBAIK Food Systems Ltd. website and all its materials
+            including but not limited to the design, layout, look, appearance,
+            graphics, content, logos, trademarks, programs and services
+          </li>
+          <li>
+            Company: ALBAIK Food Systems Company Ltd. its sister companies and
+            licensed franchisees [Aquat Food Production Plant, Express Foods
+            Company]
+          </li>
+          <li>
+            Content / Information: All text, information, photos, polls, data
+            and designs, including graphic images, buttons, displays and
+            programming on the site
+          </li>
+          <li>
+            User: Anyone entering, clicking onto, visiting and / or browsing the
+            site
+          </li>
+          <li>
+            Subscriber: Anyone registering, commenting and or participating in
+            the site including registration and as approved by the website admin
+          </li>
+          <li>
+            Submissions: All content submitted by users and subscriber to the
+            site
+          </li>
+          <li>
+            Sister Companies / Franchisees: ALBAIK Food Systems Company Ltd.
+            sister companies and licensed franchisees and authorized entities as
+            such. Only companies authorized and licensed are ALBAIK Food
+            Systems, ELBAIK Food Systems, Aquat Food Production and Express
+            Foods Company Limited. Should you be aware of any organization
+            claiming itself as a franchisee, owner, partner please contact the
+            ALBAIK legal team directly.
+          </li>
+        </ul>
+
+        <h1>Definitions</h1>
+        <p>
+          By entering, browsing and / or using this website you are agreeing to
+          comply with and be bound by the following terms and conditions of use;
+          please read our terms of usage and policies carefully by continuing on
+          using the site you are agreeing to abide by all the regulations and
+          our privacy and usage policy. If you disagree with any part of these
+          terms and conditions, please do not use our website. We reserve the
+          right to amend the website Terms and Conditions / Disclaimer from time
+          to time without notice and at our discretion. It is your
+          responsibility to review this page periodically for updates, which
+          shall come into effect upon posting. Your use of the site constitutes
+          acceptance of any / all Terms and Conditions. We reserve the right to
+          change, modify, suspend or discontinue any feature, link, content,
+          design, program of the site at anytime without due notice. We may also
+          impose limits on certain features, programs or restrict your access to
+          parts of or the entire site without notice or liability. The site is
+          operated by ALBAIK Food Systems Company Ltd. the owner of the ALBAIK
+          brand as registered under the trade license number 4040135728, having
+          its address at Tahlia Street, P.O. Box 54895, Jeddah 21524, Kingdom of
+          Saudi Arabia
+        </p>
+
+        <h1>Ownership</h1>
+        <p>
+          Material from www.albaik.com; and or any other World Wide Web site
+          owned, operated, licensed or controlled by ALBAIK Food Systems or its
+          sister companies or affiliates may not be copied, distributed,
+          republished, uploaded, posted, or transmitted in any way, without the
+          prior written consent of ALBAIK; EXCEPT: you may browse and download
+          content from the website on one computer [personal and not for any
+          business, trade entity, network / computer sharing entities, other
+          websites, social sites, electronic media, print other commercial or
+          public domains or arenas] for your personal, non-commercial home use
+          only, provided you do not delete or change any copyright, trademark,
+          or other proprietary notices. Modification or use of the materials for
+          any other purpose violates ALBAIK’s intellectual property rights. All
+          materials on the website, including but not limited to audio, images,
+          software, text, and video clips (Contents), are protected by copyright
+          and trademark laws and regulations. All rights on the website not
+          expressly granted herein are reserved. You agree to follow all
+          instructions on the website limiting the way you may use the contents.
+          There are a number of proprietary logos, brands, graphics and
+          trademarks found on the website. By making them available on the
+          website, the Company [ALBAIK] is not granting you any license for
+          utilization. Any unauthorized use of the contents and branding,
+          including but not limited to distribution, reproduction, modification,
+          display or transmission without the prior written consent of the
+          Company [ALBAIK] is in violation of the copyright laws and trademark
+          infringement and can result in legal action being taken against any
+          violators as such. You may not resell, decompile, reverse engineer,
+          disassemble or otherwise convert any material [software, content,
+          graphics, imagery etc.] to any other format.
+        </p>
+
+        <h1>Website Usage</h1>
+        <p>
+          You are permitted to download content from the website for personal
+          non-commercial use only; as per the following conditions:
+        </p>
+        <ol>
+          <li>
+            Abide by all laws and regulations and provision in utilizing the
+            Website.
+          </li>
+          <li>
+            Submissions and recommendations shared on the website or sent via
+            the website comment sections are considered voluntary and will
+            become the property of ALBAIK Food Systems Ltd., subject only to the
+            terms and conditions on the website policy. ALBAIK shall have the
+            right to copy, distribute, transmit, perform, display, modify, adapt
+            and otherwise use the submission in any way or form for any purpose
+            whatsoever (including and without limitation in the development,
+            manufacturing, marketing and services of products and services by
+            utilizing or incorporating parts or all of the submission be it the
+            overall idea, parts of or process) in all forms internally and
+            externally including without limitation by means of traditional
+            media, print media online and other electronic media and sales,
+            marketing and any other form or process of communication globally.
+            ALBAIK assumes no responsibility of confidentiality, explicit or
+            implied with respect to any submission received and bears no
+            liability for the use or disclosure of your submission as
+            confidential. There will be no compensation for any submission
+            monetary, recognition or any other manner. All submissions are
+            voluntarily shared and agreed as such. While ALBAIK values customer
+            feedback ALBAIK does not accept unsolicited ideas or suggestions.
+          </li>
+          <li>
+            As a subscriber / user of the site any content submission from your
+            end must conform with all applicable laws and regulations of the
+            Kingdom of Saudi Arabia including the following: you own or have
+            necessary licenses, rights, permissions for any material uploaded on
+            your behalf, any content uploaded does not violate any copyrights,
+            trademark infringements; not to include in your submissions any
+            content that is unlawful, harmful, threatening, abusive, harassing,
+            defamatory, vulgar, pornographic, offensive, indecent, libellous,
+            invasive of anyone’s privacy, racially or ethnically or otherwise
+            objectionable and could result in legal action with the company
+            updating the local authorities and supporting any investigations as
+            such. The company has the right to modify or delete any submissions
+            or uploads or storage for any 3rd party entity as it sees fit.
+          </li>
+          <li>
+            Should you choose to have your personal information or name publicly
+            shared on the website or to any other user website admin and company
+            bear no responsibility and it will be at the user / subscribers own
+            discretion and responsibility.
+          </li>
+          <li>
+            We reserve the right to stop this website in whole or in part, on or
+            off any service from, or block in any area at our own discretion,
+            and without prior notice, we also have the right to impose any
+            financial fees or dues should we see it permitting on any set basis.
+            We bear no legal or financial responsibility for any blocking or
+            stopping of the website at any time and for any reason as set by the
+            company
+          </li>
+        </ol>
+
+        <h1>Website Usage Rules</h1>
+        <ul>
+          <li>
+            Website explicitly not be used for any trade or advertising usage;
+            it may not be used for any trade benefits or means of commercial
+            gain.
+          </li>
+          <li>
+            You may not distribute any part of the website, content and design
+            or amend or copy [as specified above] or reuse it or reproduce any
+            whole or part of or publish or license or broadcast or transmit or
+            reengineer or reproduce parts of or copy the design, content or
+            graphics to any other website without written consent from ALBAIK
+            Food Systems Company Ltd. for the specific purpose. It is also
+            prohibited to extract information or content from this website on a
+            regular basis for information gathering or categorization or
+            database or proof building directly or indirectly without written
+            consent from ALBAIK Food Systems Company Ltd.
+          </li>
+          <li>
+            Its prohibited to utilize the website in violation of peoples
+            rights, or to gather personal information from other users.
+          </li>
+          <li>
+            You may not use the site to transmit or send any / all viruses,
+            spyware, hacking. You are also prohibited from using or sharing
+            files or software that can in any way be harmful or affect the
+            website or any computer.
+          </li>
+          <li>
+            You may not use the website in a manner that stops or hinders its
+            usage by others, you may not at anytime interfere in the website
+            operation process, its network or hinder its progress or service
+          </li>
+          <li>
+            You may not make any changes or amendments to any part or whole of
+            the website
+          </li>
+        </ul>
+
+        <h1>Registration / Subscription</h1>
+        <p>
+          You may be required to register on the website to reach certain areas
+          of the website, the company has the right to initiate registration
+          requests at any time it deems necessary, the company also has the
+          right to accept or reject any subscription or account opening on the
+          website without giving any reasoning. You are not allowed to use any
+          name or email address for any person other than yourself or use anyone
+          else’s account for any reason. If any of the services requires you to
+          open an account, you must complete the registration process by
+          providing us with current, complete and accurate information as
+          prompted by the applicable registration form. You also will choose a
+          password and a user name. You are entirely responsible for maintaining
+          the confidentiality of your password and account. Furthermore, you are
+          entirely responsible for any and all activities that occur under your
+          account. You must notify the Company immediately of any unauthorized
+          use of your account or any other breach of security. ALBAIK will not
+          be liable for any loss that you may incur as a result of someone else
+          using your password or account, either with or without your knowledge.
+          However, you could be held liable for losses incurred by ALBAIK or
+          another party due to someone else using your account or password
+          resulting in financial issues. We reserve the right to block your
+          access to the site for any reason and / or at anytime without any
+          prior notice, we have the right to delete, cancel and suspend all
+          information you may post or register as such without any prior notice
+          and prohibit your access to any data, information or files you may
+          have uploaded.
+        </p>
+
+        <h1>General Rules</h1>
+        <ul>
+          <li>
+            We confirm that any / all information we have uploaded on the site
+            is accurate and true however we are not fallible and have put in
+            effort to avoid any mistakes and strive to amend. We have made
+            reasonable efforts to avoid any mistakes as such, however should you
+            come across any information you feel may not be accurate on the site
+            please don’t hesitate to send us your comments as such immediately.
+          </li>
+          <li>
+            Under any circumstances the Company will not be held responsible for
+            any direct, indirect, consequential or original damages that may
+            result from the use or inability to use this site or any materials
+            or contents of it, even if we didn’t previously allude to it as
+            such.
+          </li>
+          <li>
+            Links and banners from others sites leading to this site may be
+            found on the web such as hot sites etc. We are not responsible for
+            those sites in any way, and we cannot know the validity of the
+            information they share even if it claims to be from or shared by an
+            employee [existing or previous of ALBAIK] we are not responsible for
+            the accuracy or reliability of such information.
+          </li>
+          <li>
+            You may be directed to links to sites not owned by the Company /
+            subsidiaries. Such links are of services provided to you; however we
+            have no control over such sites and are in no way to be held
+            responsible for liable by such sites or your usage of them. We are
+            not liable for any content provided to such sites, and disclaim any
+            legal responsibility for your usage and browsing of the site
+            directly or indirectly.
+          </li>
+          <li>
+            We are not responsible for any of our products being featured in
+            sites not owned by us and don’t approve as such.
+          </li>
+          <li>
+            The website may contain logos / branding or allude to company names,
+            logos, branding, slogans and or products for other commercial
+            entities and these trademarks and names are the property of the
+            respective trademark owners.
+          </li>
+          <li>
+            All menu and product prices mentioned on the site can be amended,
+            modified and changed without previous notice, this includes
+            standards and content.
+          </li>
+          <li>
+            There may be additional terms and conditions and updates for some
+            and / or all services or select geographical locations; with such
+            terms and conditions being featured for those benefiting from it.
+            Such terms and conditions are still in effect within the framework
+            of the original terms and conditions of this site.
+          </li>
+        </ul>
+
+        <h1>Privacy</h1>
+        <p>
+          Any personal information you upload / transmit to the website through
+          any means will be utilized by the Company in accordance with set
+          privacy policy of the site and as the Site by any means, will be
+          utilized by the Company in accordance with the privacy policy of the
+          site, and any other material you transmit to the website will be
+          treated as non-confidential.
+        </p>
+
+        <h1>Compensation</h1>
+        <p>
+          Violation of any of these conditions, whether through the use of the
+          site and / or services associated with it or violating the rights of
+          others, including, but not limited to (rights to publishing and trade
+          secrets, privacy, trademark) and those rights associated with the
+          other parties, and that any abuse of any of these conditions the
+          violator will bear all expenses and costs incurred by the Company
+          during and throughout any legal prosecution and handling.
+        </p>
+
+        <h1>Independence of the text</h1>
+        <p>
+          If any of the terms and conditions set forth in this agreements or any
+          points mentioned come about to be unenforceable or non-implementable
+          based on special judicial entities input for any reason will not in
+          any means have any effect on the other points and conditions within
+          the terms and conditions agreement of this site or any legal effect;
+          and the set cancellation will be limited in the particular case or
+          incident and no party can try to affix to it as such even if such a
+          cancellation precedes these terms and conditions or comes post.
+        </p>
+
+        <h1>Jurisdiction</h1>
+        <p>
+          The terms and conditions set in this Website are all governed,
+          construed and enforced according to the laws of the Kingdom of Saudi
+          Arabia, and in the event of any disagreement or dispute in the
+          application or interpretation of any provision of these conditions
+          shall be referred to and heard by the Saudi Arabian judicial
+          authority.
+        </p>
+      </DataCard>
+    </div>
+  );
 };
 
 export default page;
