404ed2cec840c4f3fcea102cf9d7089bbed31859
Alex6n <hheikal.work@gmail.com>
2024-01-18 23:36:45 +0200

    Add DataCard Component

---
+++ components/DataCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DataCard = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="bg-primary/85 md:px-20 font-bold data-card p-16 md:rounded-xl md:mx-12 lg:mx-24 xl:mx-28 my-10">
+      {children}
+    </div>
+  );
+};
+
+export default DataCard;
