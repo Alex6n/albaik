28bacb810c0246b45b1ce0784a8b5db6440e17c5
Alex6n <hheikal.work@gmail.com>
2024-01-27 18:26:19 +0200

    munu schema pending actual data

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
   return (
     <div className="my-14 grid justify-center text-center">
       <div className="px-5 border-[2px] border-dashed border-accent/20 rounded-3xl">
-        <Image src="/albaik3.png" alt="404" height={350} width={350} />
+        <Image src="/icons/albaik3.png" alt="404" height={350} width={350} />
         <h2 className="text-[60px] font-bold text-accent/70 stroke mt-2 pt-5 border-t border-zinc-300">
           <span className="block leading-6">404</span>
           Not Found
