8a79facda16fcf93420108d7dfd8cb0e995e1238
Alex6n <hheikal.work@gmail.com>
2024-01-27 21:26:28 +0200

    add avalibilty badge to MenuList

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,5 +1,7 @@
 import Catogeries from "@/components/Menu/Catogeries";
 import { ImageSlider } from "@/components/Menu/ImageSlider";
+import MenuList from "@/components/Menu/MenuList";
+import * as menu from "../../public/menu.json";
 
 const images = ["/offers/offer1.jpg", "/offers/offer2.jpg"];
 
@@ -15,8 +17,24 @@ const categories = [
 ];
 
 const page = () => {
+  const items = [
+    {
+      image: "/landing/home_hero.jpg",
+      title: "delecious chicken baby",
+      link: "/item1",
+      highlighted: false,
+      available: true,
+    },
+    {
+      image: "/landing/home_hero.jpg",
+      title: "delecious chicken baby1",
+      link: "/item1",
+      highlighted: false,
+      available: false,
+    },
+  ];
   return (
-    <section className="min-h-[53.9vh]">
+    <section>
       <div className="bg-[#CE3737]/90 py-5 text-background font-bold text-center text-7xl">
         Menu
       </div>
@@ -24,6 +42,7 @@ const page = () => {
         <div className="max-w-[1035px]">
           <ImageSlider images={images} />
           <Catogeries catogeries={categories} />
+          <MenuList items={items} />
         </div>
       </div>
     </section>
--- components/Menu/MenuList.tsx
+++ components/Menu/MenuList.tsx
@@ -1,40 +1,51 @@
 import { cn } from "@/lib/utils";
+import { TimerIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 interface MenuListProps {
-  items: [
-    {
-      image: string;
-      title: string;
-      link: string;
-      highlighted: boolean;
-    }
-  ];
+  items: {
+    image: string;
+    title: string;
+    link: string;
+    highlighted: boolean;
+    available: boolean;
+  }[];
 }
 
 const MenuList = ({ items }: MenuListProps) => {
   return (
-    <div className="w-full grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:justify-center gap-1">
+    <div className="w-full grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:justify-center gap-2">
       {items.map((item) => (
         <Link
           key={item.title}
           href={item.link}
-          className={cn(
-            "h-[200px] relative rounded-lg overflow-hidden hover:opacity-50 transition-opacity",
-            { "col-span-2": item.highlighted }
-          )}
+          className={cn("h-[200px] relative rounded-lg overflow-hidden", {
+            "col-span-2": item.highlighted,
+          })}
         >
-          <div className="bg-gradient-to-b h-[30%] w-full to-black/60 from-transparent absolute bottom-0">
-            <h1 className="text-zinc-100 mx-2 mt-7">{item.title}</h1>
+          {item.available || (
+            <div className="absolute z-10 text-xs text-red-800 bg-gray-100/80 rounded-xl px-2 py-1 m-2 hover:opacity-100">
+              <TimerIcon className="inline w-3 h-3 mr-1" />
+              Unavaliable
+            </div>
+          )}
+          <div
+            className={`${
+              item.available ? "" : "opacity-35"
+            } hover:opacity-50 transition-opacity w-full h-full`}
+          >
+            <div className="bg-gradient-to-b h-[30%] w-full to-black/60 from-transparent absolute bottom-0">
+              <h1 className="text-zinc-100 mx-2 mt-7">{item.title}</h1>
+            </div>
+            <Image
+              src={item.image}
+              alt={item.title}
+              fill
+              className="object-cover -z-10"
+            />
           </div>
-          <Image
-            src={item.image}
-            alt={item.title}
-            fill
-            className="object-cover -z-10"
-          />
         </Link>
       ))}
     </div>
