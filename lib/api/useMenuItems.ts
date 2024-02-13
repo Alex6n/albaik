74d50a5e81dde0415c431823beb7b1eeaa72b221
Hamza Heikal <hheikal.work@gmail.com>
2024-02-14 01:09:50 +0300

    add useMenuItem for singular item

--- lib/api/useMenuItems.ts
+++ lib/api/useMenuItems.ts
@@ -1,3 +1,4 @@
+import { MenuItemType } from "@/components/Menu/MenuItem";
 import { useQuery } from "@tanstack/react-query";
 
 export function useMenuItems() {
@@ -5,5 +6,18 @@ export function useMenuItems() {
     queryKey: ["menuItems"],
     queryFn: async () =>
       fetch("http://localhost:4000/menu").then((res) => res.json()),
+  });
+}
+
+export function useMenuItem(itemId: string) {
+  return useQuery({
+    queryKey: ["menuItem", itemId],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:4000/menu");
+      const data = await response.json();
+      return data.find(
+        ({ item }: { item: MenuItemType }) => item.id === itemId
+      );
+    },
   });
 }
